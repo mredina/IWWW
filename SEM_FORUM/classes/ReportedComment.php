b1eeeeb1415a2d65438fc4c3811eaa04ef1ddd34
st55428 <st55428@upce.cz>
2021-01-20 22:44:29 +0100

    class reportedComment

---
+++ SEM_FORUM/classes/ReportedComment.php
@@ -0,0 +1,53 @@
+<?php
+
+class ReportedComment{
+
+    private $conn = null;
+
+    public function __construct()
+    {
+        $this->conn = Connection::getConnection();
+    }
+
+
+    public function getAllReportedComments()
+    {
+        $stmt = $this->conn->prepare("SELECT rc.*, t.id as threadId, c.id as categoryId FROM reportedComments rc JOIN comments com ON rc.commentId = com.id JOIN threads t ON t.id = com.threadId JOIN categories c ON c.id = t.categoryId");
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    public function insertReason($idComment,$text,$textComment)
+    {
+
+        $user_name = Authentication::getInstance()->getIdentity()['email'];
+
+
+        $stmt = $this->conn->prepare("INSERT INTO reportedComments (commentId,email, reason,text) VALUES (:commentId, :email, :reason,:textComment)");
+        $stmt->bindParam(":commentId", $idComment);
+        $stmt->bindParam(":email", $user_name);
+        $stmt->bindParam(":reason", $text);
+        $stmt->bindParam(":textComment", $textComment);
+        try {
+            $stmt->execute();
+        } catch (PDOException $e) {
+            return false;
+        }
+        return true;
+    }
+
+    public function deleteReason($id)
+    {
+        $stmt = $this->conn->prepare("DELETE FROM reportedComments WHERE id = :id");
+        $stmt->bindParam(":id", $id);
+
+        try {
+            $stmt->execute();
+
+        } catch (PDOException $e) {
+            return false;
+        }
+        return true;
+    }
+}
+?>
